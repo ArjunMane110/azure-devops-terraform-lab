4eabb540b7bb9edcbe908f317e08fc7a2448eb7e
arjunmane110 <arjunmane110@protonmail.com>
2026-03-12 20:45:28 +0530

    Fix win25 size - use available SKU for Central US

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -36,7 +36,7 @@ resource "azurerm_windows_virtual_machine" "win-vm" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2022-Datacenter"
+    sku       = "2025-Datacenter"
     version   = "latest"
   }
 
