c5afd16e30a806273cb3ea67062ba850f8ad9e33
arjunmane110 <arjunmane110@protonmail.com>
2026-03-13 23:15:43 +0530

    Mirror dev modules to prod environment - fixes #3

---
+++ environments/prod/variables.tf
@@ -0,0 +1,58 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "vnet_address_space" {
+  type = list(string)
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "subnet_prefix" {
+  type = string
+}
+
+variable "vm_name" {
+  type = string
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_D2s_v3"
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "create_public_ip" {
+  type    = bool
+  default = false
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "nsg_name" {
+  type = string
+}
