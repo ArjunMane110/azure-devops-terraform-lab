3b0b1dd6fd291a7d3279053cbcc5bd7c82e5a1aa
arjunmane110 <arjunmane110@protonmail.com>
2026-03-24 09:44:37 +0530

    Remove all kv_name references replace with keyvault_name

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_key_vault" "this" {
-  name                        = var.keyvault_name
-  location                    = var.location
-  resource_group_name         = var.resource_group_name
-  tenant_id                   = var.tenant_id
-  sku_name                    = "standard"
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
-  enable_rbac_authorization   = false
+  name                       = var.keyvault_name
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  tenant_id                  = var.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+  rbac_authorization_enabled = false
 
   access_policy {
     tenant_id = var.tenant_id
