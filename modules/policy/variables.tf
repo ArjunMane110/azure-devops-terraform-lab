6559e759d60d0b95a44c8c0f93dae3f02a60986e
arjunmane110 <arjunmane110@protonmail.com>
2026-03-25 10:05:41 +0530

    Add Azure Policy module - require Environment and ManagedBy tags

---
+++ modules/policy/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_id" {
+  description = "Resource group ID to assign policy to"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment tag"
+  type        = string
+}
